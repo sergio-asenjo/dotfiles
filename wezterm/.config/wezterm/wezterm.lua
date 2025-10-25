66f5761ed4ec32e1090601b5f565d608e451818d
sergio-asenjo <20078140+sergio-asenjo@users.noreply.github.com>
2025-10-25 01:06:47 -0300

    chore: updates keybinds + wezterm fonts.

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -3,6 +3,10 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 config.color_scheme = 'Tokyo Night Moon'
+config.font = wezterm.font_with_fallback {
+  'Zenbones Mono',
+  'Noto Color Emoji'
+}
 
 wezterm.on('format-tab-title', function(tab, tabs, panes, config, hover, max_width)
   local pane = tab.active_pane
