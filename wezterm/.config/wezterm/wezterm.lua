291b47300191d6ee029538dba98c48f759b91a85
sergio-asenjo <20078140+sergio-asenjo@users.noreply.github.com>
2025-12-08 22:25:46 -0300

    chore: style/color changes.
    Changing styles and colors to something with higher contrast
    because holy crap do I have a lot of ambiance light in my
    office.

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -2,7 +2,37 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
-config.color_scheme = 'Tokyo Night Moon'
+config.colors = {
+  foreground = '#ffffff',
+  background = '#0a0e27',
+  cursor_bg = '#ffffff',
+  cursor_border = '#ffffff',
+  cursor_fg = '#0a0e27',
+  selection_bg = '#4a6fa5',
+  selection_fg = '#ffffff',
+  
+  ansi = {
+    '#1a1a2e', -- black
+    '#ff4444', -- red
+    '#44ff44', -- green
+    '#ffdd44', -- yellow
+    '#4488ff', -- blue
+    '#ff44ff', -- magenta
+    '#44ffff', -- cyan
+    '#ffffff', -- white
+  },
+  brights = {
+    '#555555', -- bright black
+    '#ff6666', -- bright red
+    '#66ff66', -- bright green
+    '#ffff66', -- bright yellow
+    '#6699ff', -- bright blue
+    '#ff66ff', -- bright magenta
+    '#66ffff', -- bright cyan
+    '#ffffff', -- bright white
+  },
+}
+
 config.font = wezterm.font_with_fallback {
   'Zenbones Mono',
   'Noto Color Emoji'
